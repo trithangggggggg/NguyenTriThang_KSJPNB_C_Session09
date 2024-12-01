3f1085d238ff261fe7e92f785da81187a1713562
trithangggggggg <trithang2006vn@gmail.com>
2024-12-01 23:39:50 +0700

    Add files via upload

---
+++ Session9_Ex1.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int array[100];
+    int n, value, addindex;
+
+    printf("Nhap so phan tu muon nhap: ");
+    scanf("%d", &n);
+
+    if (n > 100 || n <= 0) {
+        printf("So phan tu khong hop le\n");
+        return 1;
+    }
+
+    printf("Nhap cac phan tu:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Phan tu thu %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    printf("Nhap gia tri can chen: ");
+    scanf("%d", &value);
+
+    printf("Nhap vi tri muon chen (1-%d): ", n + 1);
+    scanf("%d", &addindex);
+
+    if (addindex < 1 || addindex > n + 1) {
+        printf("Vi tri khong hop le\n");
+        return 1;
+    }
+
+    for (int i = n; i >= addindex; i--) {
+        array[i] = array[i - 1];
+    }
+    array[addindex - 1] = value;
+    n++;
+
+    printf("Mang sau khi chen: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
